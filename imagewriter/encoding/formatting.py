8634934dee4af7212b707812d100dfb861f228b2
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-13 12:23:06 -0800

    page length

--- imagewriter/encoding/formatting.py
+++ imagewriter/encoding/formatting.py
@@ -3,9 +3,10 @@ from imagewriter.encoding.base import esc, format_number
 from imagewriter.encoding.length import Inch, Length
 
 LINE_WIDTH = Inch(8)
+LENGTH_CONVERSION = 144  # Units per inch
 
 
-def set_left_margin(distance: Length | int, pitch: Pitch) -> bytes:
+def set_left_margin(width: Length | int, pitch: Pitch) -> bytes:
     """
     Set the left margin, as per page 59 of the ImageWriter II Technical
     Reference Manual.
@@ -14,11 +15,27 @@ def set_left_margin(distance: Length | int, pitch: Pitch) -> bytes:
     conversion: float = (pitch.characters_per_line - 1) / LINE_WIDTH.inches
     set_to: int = 0
 
-    if isinstance(distance, Length):
-        set_to = int(distance.inches * conversion)
+    if isinstance(width, Length):
+        set_to = int(width.inches * conversion)
     else:
-        set_to = distance
+        set_to = width
 
     set_to = min(set_to, pitch.characters_per_line - 1)
 
     return esc("L", format_number(set_to, 3))
+
+
+def set_page_length(length: Length | int) -> bytes:
+    """
+    Set the page length, as per page 61 of the ImageWriter II Technical
+    Reference Manual.
+    """
+
+    set_to: int = 0
+
+    if isinstance(length, Length):
+        set_to = int(length.inches) * LENGTH_CONVERSION
+    else:
+        set_to = length
+
+    return esc("H", format_number(set_to, 4))
