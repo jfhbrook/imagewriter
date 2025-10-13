21b7624d4bda2a38dcbcae272a32790bf2ca16bf
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-12 23:58:06 -0800

    Fix root encoding exports

--- imagewriter/encoding/__init__.py
+++ imagewriter/encoding/__init__.py
@@ -1,12 +1,41 @@
+from typing import List
+
 from imagewriter.encoding.base import ctrl, esc
+from imagewriter.encoding.character import CharacterEncoder, Text
+from imagewriter.encoding.format import (
+    START_BOLDFACE,
+    START_DOUBLE_WIDTH,
+    START_HALF_HEIGHT,
+    START_SUBSCRIPT,
+    START_SUPERSCRIPT,
+    START_UNDERLINE,
+    STOP_BOLDFACE,
+    STOP_DOUBLE_WIDTH,
+    STOP_HALF_HEIGHT,
+    STOP_SUBSCRIPT,
+    STOP_SUPERSCRIPT,
+    STOP_UNDERLINE,
+)
 from imagewriter.encoding.language import Language
-from imagewriter.encoding.special import SpecialCharacterEncoder
 from imagewriter.encoding.switch import SoftwareSwitch
 
-__all__ = [
+__all__: List[str] = [
     "ctrl",
     "esc",
+    "CharacterEncoder",
+    "Text",
     "Language",
-    "SpecialCharacterEncoder",
     "SoftwareSwitch",
+    "START_DOUBLE_WIDTH",
+    "STOP_DOUBLE_WIDTH",
+    "START_UNDERLINE",
+    "STOP_UNDERLINE",
+    "START_BOLDFACE",
+    "STOP_BOLDFACE",
+    "START_HALF_HEIGHT",
+    "STOP_HALF_HEIGHT",
+    "START_SUPERSCRIPT",
+    "STOP_SUPERSCRIPT",
+    "START_SUBSCRIPT",
+    "STOP_SUBSCRIPT",
 ]
