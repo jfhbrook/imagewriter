8d43f305e83aff6b9510f6273539a99e5c641732
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-13 15:02:25 -0800

    Additional notes on insertions

--- imagewriter/encoding/insertion.py
+++ imagewriter/encoding/insertion.py
@@ -6,6 +6,10 @@ return insertion is enabled, an CR (\\r) will be inserted before every LF (\\n)
 or FF (^L) character. This is enabled by default.
 
 Note that this does not control whether or not LF or FF will trigger printing.
+
+Note that this is also different from automatic LF insertion after a CR, the
+opposite behavior. This is controlled by switches, as per page 77 of the
+ImageWriter II Technical Reference Manual.
 """
 
 ENABLE_CARRIAGE_RETURN_INSERTION = esc("l1")
