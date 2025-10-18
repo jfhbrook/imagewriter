c5d776f36c366b2c0241951a6bfa295724707569
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-17 18:08:01 -0800

    Refactor container

--- imagewriter/container.py
+++ imagewriter/container.py
@@ -1,32 +1,29 @@
-from typing import Any, Callable, Dict, Protocol, Self
+from typing import Protocol, Self
 
 from imagewriter.serial import Serial
 from imagewriter.switch import DIPSwitchSettings, SoftwareSwitchSettings
 
-SoftwareSwitchesFactory = Callable[[DIPSwitchSettings], SoftwareSwitchSettings]
+
+class SoftwareSwitchesFactory(Protocol):
+    def __call__(
+        self: Self, dip_switches: DIPSwitchSettings
+    ) -> SoftwareSwitchSettings: ...
 
 
-def default_software_switches(
+class SerialFactory(Protocol):
+    def __call__(self: Self, port: str, dip_switches: DIPSwitchSettings) -> Serial: ...
+
+
+def software_switches_factory(
     dip_switches: DIPSwitchSettings,
 ) -> SoftwareSwitchSettings:
     return SoftwareSwitchSettings.defaults(dip_switches)
 
 
-SerialKwargsFactory = Callable[[str, DIPSwitchSettings], Dict[str, Any]]
-
-
-def default_serial_kwargs(port: str, dip_switches: DIPSwitchSettings) -> Dict[str, Any]:
-    return dict(
+def serial_factory(port: str, dip_switches: DIPSwitchSettings) -> Serial:
+    return Serial(
         port=port, baudrate=dip_switches.baud_rate, protocol=dip_switches.protocol
     )
-
-
-class SerialFactory(Protocol):
-    def __call__(self: Self, **kwargs: Any) -> Serial: ...
-
-
-def default_serial(**kwargs: Any) -> Serial:
-    return Serial(**kwargs)
 
 
 class Container:
@@ -34,10 +31,9 @@ class Container:
         self: Self,
         port: str,
         dip_switches: DIPSwitchSettings = DIPSwitchSettings.defaults(),
-        software_switches: SoftwareSwitchesFactory = default_software_switches,
-        serial_kwargs: SerialKwargsFactory = default_serial_kwargs,
-        serial: SerialFactory = default_serial,
+        software_switches: SoftwareSwitchesFactory = software_switches_factory,
+        serial: SerialFactory = serial_factory,
     ) -> None:
         self.dip_switches: DIPSwitchSettings = dip_switches
         self.software_switches: SoftwareSwitchSettings = software_switches(dip_switches)
-        self.port: Serial = serial(**serial_kwargs(port, dip_switches))
+        self.port: Serial = serial(port, dip_switches)
