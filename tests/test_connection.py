919680a2f644139e4a2b6c4d08819c1ad0577a28
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-21 14:40:56 -0800

    Connection apparently Just Works??

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,5 +1,28 @@
+import time
+from typing import Any
+from unittest.mock import call
+
 from imagewriter.connection import Connection
+from imagewriter.encoding.base import Bytes
 
 
-def test_connection(connection: Connection) -> None:
-    pass
+def test_connection(serial: Any, connection: Connection) -> None:
+    def sleep(n: int | float) -> None:
+        time.sleep(2 * n * connection._timeout)
+
+    first = Bytes(b"first")
+    second = Bytes(b"second")
+    interrupt = Bytes(b"interrupt")
+
+    connection.write([first])
+
+    sleep(1)
+
+    serial.cts = False
+
+    connection.write([second])
+    connection.interrupt([interrupt])
+
+    sleep(2)
+
+    serial.write.assert_has_calls([call(bytes(first)), call(bytes(interrupt))])
