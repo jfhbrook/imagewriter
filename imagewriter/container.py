3594962a13f2391654658f8e1a00769afb6e4594
Josh Holbrook <josh.holbrook@gmail.com>
2025-10-23 19:38:25 -0800

    Push the peas on Container a little

--- imagewriter/container.py
+++ imagewriter/container.py
@@ -1,5 +1,7 @@
 from typing import Optional, Self
 
+from serial.tools.list_ports import comports
+
 from imagewriter.connection import Connection
 from imagewriter.serial import Serial
 from imagewriter.settings import Settings
@@ -9,17 +11,45 @@ from imagewriter.switch import DIPSwitches
 class Container:
     def __init__(
         self: Self,
-        port: str,
-        dip_switches: DIPSwitches = DIPSwitches.defaults(),
+        port: Optional[str] = None,
+        dip_switches: Optional[DIPSwitches] = None,
     ) -> None:
-        self._port = port
-        self.dip_switches: DIPSwitches = dip_switches
+        self._port: Optional[str] = port
+        self.dip_switches: DIPSwitches = (
+            dip_switches if dip_switches else self.create_dip_switches()
+        )
 
         self._settings: Optional[Settings] = None
         self._serial: Optional[Serial] = None
         self._connection: Optional[Connection] = None
 
+    def create_dip_switches(self: Self) -> DIPSwitches:
+        """
+        Create a DIPSwitches object.
+        """
+
+        return DIPSwitches.defaults()
+
+    def create_port(self: Self) -> str:
+        """
+        Create a port.
+        """
+
+        return comports()[-1].device
+
+    def reload_port(self: Self) -> None:
+        """
+        Reload the port, and any of its dependents.
+        """
+
+        if self._serial:
+            self.reload_serial()
+
     def create_settings(self: Self) -> Settings:
+        """
+        Create a new Settings object.
+        """
+
         return Settings.replace(
             Settings.defaults(self.dip_switches),
             lf_when_line_full=True,
@@ -28,9 +58,17 @@ class Container:
         )
 
     def reload_settings(self: Self) -> None:
+        """
+        Reload the settings, and any of their dependents.
+        """
+
         pass
 
     def create_serial(self: Self) -> Serial:
+        """
+        Create a new Serial object.
+        """
+
         return Serial(
             port=self.port,
             baudrate=self.dip_switches.baud_rate,
@@ -38,30 +76,46 @@ class Container:
         )
 
     def reload_serial(self: Self) -> None:
+        """
+        Reload the Serial object, and any of its dependents.
+        """
+
         self.serial.close()
         self.serial.port = self.port
         self.serial.open()
+        self.reload_connection()
 
     def create_connection(self: Self) -> Connection:
+        """
+        Create a new Connection object.
+        """
+
         return Connection(self.serial)
 
     def reload_connection(self: Self) -> None:
+        """
+        Reload the Connection object, and any of its dependents.
+        """
+
+        # The Connection object already has a reference to the Serial
+        # object, which has not been modified.
         pass
 
     @property
     def port(self: Self) -> str:
+        if not self._port:
+            self._port = self.create_port()
         return self._port
 
     @port.setter
     def port(self: Self, port: str) -> None:
         self._port = port
-        self.reload_serial()
+        self.reload_port()
 
     @property
     def settings(self: Self) -> Settings:
         if not self._settings:
             self._settings = self.create_settings()
-
         return self._settings
 
     @settings.setter
